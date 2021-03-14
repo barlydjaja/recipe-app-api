ce9607ca3497bbde3b69dd2db4d07b361c6400e8
barlydjaja <barlydjaja@gmail.com>
2021-03-14 11:43:39 +0700

    added manage user endpoint

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -107,3 +107,45 @@ class PublicUserApiTests(TestCase):
         """Test that authentication is required for users"""
         res = self.client.get(ME_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API Request that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='barlydjaja@gmail.com',
+            password='testuser123',
+            name='barly'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in used"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating user profile for authenticated user"""
+        payload = {
+            'name': 'new name',
+            'password': 'newpassowrdss',
+        }
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
